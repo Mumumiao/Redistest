f28d5c5b8d841bea91823ec6f03a3dcf27104c2e
木木淼 <18240597266@163.com>
2022-08-27 11:10:29 +0800

    Initial commit

---
+++ src/test/java/Redistest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+public class Redistest {
+    @Test
+    public void test(){
+        Jedis jedis=new Jedis("127.0.0.1",6379);
+        System.out.println(jedis.ping());
+//    jedis.sadd("testset",new String[]{"jey","汤姆1","还买暖宝宝1"});
+//        jedis.lrange("testset",0,jedis.scard("testset")-1).forEach(d->{
+//
+//            System.out.println(d);
+//        });
+        System.out.println(jedis.sadd("testset","je"));
+        jedis.smembers("testset").forEach(d->{
+            System.out.println(d);
+        });
+    }
+
+}
